722bad72311379f65476a7436cb4f9e27eab3fc9
kanadeiar <kanadeiar@gmail.com>
2024-01-21 00:58:48 +0300

    Завершение первой задачи

--- src/lesson1/Lesson1Anketa/Program.cs
+++ src/lesson1/Lesson1Anketa/Program.cs
@@ -1,9 +1,24 @@
 ﻿using Kanadeiar.Common;
+using Lesson1Anketa.AnketaFeat;
+
 ConsoleHelper.PrintHeader("Задача № 1", "Задача № 1. Написать программу «Анкета».");
 
+var anketa = new Anketa();
 
+anketa.SurName = ConsoleHelper.ReadLineFromConsole("Введите фамилию");
+anketa.Name = ConsoleHelper.ReadLineFromConsole("Введите имя");
+anketa.Age = ConsoleHelper.ReadNumberFromConsole<int>("Введите возраст");
+anketa.Height = ConsoleHelper.ReadNumberFromConsole<int>("Введите рост");
+anketa.Weight = ConsoleHelper.ReadNumberFromConsole<int>("Введите вес");
 
+Console.WriteLine("\nСклеивание:");
+Console.WriteLine(anketa.GetGluedLine());
 
+Console.WriteLine("\nФорматирование:");
+Console.WriteLine(anketa.GetFormatted());
+
+Console.WriteLine("\nИнтерполяция:");
+Console.WriteLine(anketa.GetInterpolated());
 
 ConsoleHelper.PrintFooter();
 
