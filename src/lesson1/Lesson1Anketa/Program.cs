bb8ba1048256be302d85718b2e45a267999ebc54
kanadeiar <kanadeiar@gmail.com>
2024-01-20 23:11:09 +0300

    Начало первой задачи

---
+++ src/lesson1/Lesson1Anketa/Program.cs
@@ -0,0 +1,10 @@
+﻿using Kanadeiar.Common;
+ConsoleHelper.PrintHeader("Задача № 1", "Задача № 1. Написать программу «Анкета».");
+
+
+
+
+
+ConsoleHelper.PrintFooter();
+
+
---
+++ tests/Lesson1Tests/AnketaTests.cs
@@ -0,0 +1,13 @@
+namespace Lesson1Tests
+{
+    public class AnketaTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+
+            Assert.True(true);
+        }
+    }
+}
