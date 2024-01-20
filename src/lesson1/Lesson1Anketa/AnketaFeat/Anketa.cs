a3d2ff50461de71fa14a200bda3eef5597f0ccdc
kanadeiar <kanadeiar@gmail.com>
2024-01-20 23:52:26 +0300

    Первая функция

---
+++ src/lesson1/Lesson1Anketa/AnketaFeat/Anketa.cs
@@ -0,0 +1,17 @@
+﻿namespace Lesson1Anketa.AnketaFeat;
+
+public class Anketa
+{
+    private string _name;
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value;
+    }
+
+    public Anketa(string name)
+    {
+        _name = name;
+    }
+}
---
+++ tests/Lesson1Tests/AnketaFeat/AnketaTests.cs
@@ -0,0 +1,16 @@
+﻿using Lesson1Anketa.AnketaFeat;
+
+namespace Lesson1Tests.AnketaFeat;
+
+public class AnketaTests
+{
+    [Fact]
+    public void TestName()
+    {
+        var anketa = new Anketa("default");
+
+        anketa.Name = "Test";
+
+        Assert.Equal("Test", anketa.Name);
+    }
+}
