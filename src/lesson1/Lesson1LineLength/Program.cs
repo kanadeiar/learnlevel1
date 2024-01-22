166f3e087ce8aa72603d8a6accda295ae3aac009
kanadeiar <kanadeiar@gmail.com>
2024-01-22 21:50:04 +0300

    Начало выполнения 3-го задания

---
+++ src/lesson1/Lesson1LineLength/Program.cs
@@ -0,0 +1,7 @@
+﻿using Kanadeiar.Common;
+
+ConsoleHelper.PrintHeader("Задача № 3", "Задача № 3. Написать программу расчета расстояния между двумя точками.");
+
+
+
+ConsoleHelper.PrintFooter();
---
+++ tests/lesson1/Lesson1LineLengthTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Lesson1LineLengthTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
