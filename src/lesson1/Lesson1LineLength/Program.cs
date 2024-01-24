39922bba7eb07a9e638f0dda0d3d328d059ed640
kanadeiar <kanadeiar@gmail.com>
2024-01-25 01:36:13 +0300

    Доработка задачи № 3 в части пользовательского интерфейса

--- src/lesson1/Lesson1LineLength/Program.cs
+++ src/lesson1/Lesson1LineLength/Program.cs
@@ -1,7 +1,22 @@
-﻿using Kanadeiar.Common;
+﻿using System.Drawing;
+using Kanadeiar.Common;
+using Lesson1LineLength.LineFeat;
 
 ConsoleHelper.PrintHeader("Задача № 3", "Задача № 3. Написать программу расчета расстояния между двумя точками.");
 
+Console.WriteLine("Начальная точка:");
+var x1 = ConsoleHelper.ReadNumberFromConsole<int>("x1");
+var y1 = ConsoleHelper.ReadNumberFromConsole<int>("y1");
 
+Console.WriteLine("Конечная точка:");
+var x2 = ConsoleHelper.ReadNumberFromConsole<int>("x2");
+var y2 = ConsoleHelper.ReadNumberFromConsole<int>("y2");
+
+var line = Line.Create(new Point(x1, y1), new Point(x2, y2));
+
+var result = line.Length();
+Console.WriteLine($"Вычисленная длинна: {result:F}");
+
+Console.WriteLine($"Сформированная строка с результатом: {line.TextLength()}");
 
 ConsoleHelper.PrintFooter();
