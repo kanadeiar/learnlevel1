1a723e3d2fef33cb2d904d89c354ca40451362bc
kanadeiar <kanadeiar@gmail.com>
2024-01-25 01:43:14 +0300

    Ввод данных

---
+++ src/lesson1/Lesson1Swap/SwapFunc/Swapper.cs
@@ -0,0 +1,30 @@
+﻿namespace Lesson1Swap.SwapFunc;
+
+public class Swapper
+{
+    private int _first;
+
+    public int First
+    {
+        get => _first;
+        set => _first = value;
+    }
+
+    private int _second;
+
+    public int Second
+    {
+        get => _second;
+        set => _second = value;
+    }
+
+    private Swapper(int first, int second)
+    {
+        _first = first;
+        _second = second;
+    }
+    public static Swapper Create(int first, int second)
+    {
+        return new Swapper(first, second);
+    }
+}
--- tests/lesson1/Lesson1SwapTests/SwapFunc/SwapperTests.cs
+++ tests/lesson1/Lesson1SwapTests/SwapFunc/SwapperTests.cs
@@ -1,19 +1,21 @@
-﻿namespace Lesson1SwapTests.SwapFunc;
+﻿using Lesson1Swap.SwapFunc;
+
+namespace Lesson1SwapTests.SwapFunc;
 
 public class SwapperTests
 {
     [Fact]
     public void TestData()
     {
-        //var swapper = Swapper.Create(5, 6);
+        var swapper = Swapper.Create(5, 6);
 
-        //Assert.Equal(5, swapper.First);
-        //Assert.Equal(6, swapper.Second);
+        Assert.Equal(5, swapper.First);
+        Assert.Equal(6, swapper.Second);
 
-        //swapper.First = 3;
-        //swapper.Second = 2;
+        swapper.First = 3;
+        swapper.Second = 2;
 
-        //Assert.Equal(3, swapper.First);
-        //Assert.Equal(2, swapper.Second);
+        Assert.Equal(3, swapper.First);
+        Assert.Equal(2, swapper.Second);
     }
 }
