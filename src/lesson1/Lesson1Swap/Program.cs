638aeefeecc16a7b7c0b62022e22a5c90eb7d85a
kanadeiar <kanadeiar@gmail.com>
2024-01-25 01:31:37 +0300

    Начало выполнения задачи № 4

---
+++ src/lesson1/Lesson1Swap/Program.cs
@@ -0,0 +1,8 @@
+﻿using Kanadeiar.Common;
+
+ConsoleHelper.PrintHeader("Задача № 2", "Задача № 2. Написать программу обмена значениями двух переменных.");
+
+
+
+
+ConsoleHelper.PrintFooter();
---
+++ tests/lesson1/Lesson1SwapTests/SwapFunc/SwapperTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Lesson1SwapTests.SwapFunc;
+
+public class SwapperTests
+{
+    [Fact]
+    public void TestData()
+    {
+        //var swapper = Swapper.Create(5, 6);
+
+        //Assert.Equal(5, swapper.First);
+        //Assert.Equal(6, swapper.Second);
+
+        //swapper.First = 3;
+        //swapper.Second = 2;
+
+        //Assert.Equal(3, swapper.First);
+        //Assert.Equal(2, swapper.Second);
+    }
+}
