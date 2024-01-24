0f14e02721e7e4b7de15027051c6990302a9d4ad
kanadeiar <kanadeiar@gmail.com>
2024-01-24 11:20:35 +0300

    Вычисление расстояния по формуле реализовано

---
+++ src/lesson1/Lesson1LineLength/LineFeat/Line.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+
+namespace Lesson1LineLength.LineFeat;
+
+public class Line
+{
+    private Point _pointStart;
+
+    public Point PointStart
+    {
+        get => _pointStart;
+        set => _pointStart = value;
+    }
+
+    private Point _pointEnd;
+
+    public Point PointEnd
+    {
+        get => _pointEnd;
+        set => _pointEnd = value;
+    }
+
+    private Line(Point pointStart, Point pointEnd)
+    {
+        _pointStart = pointStart;
+        _pointEnd = pointEnd;
+    }
+    public static Line Create(Point pStart, Point pEnd)
+    {
+        return new Line(pStart, pEnd);
+    }
+
+    public double Length()
+    {
+        var result = Math.Sqrt(Math.Pow(PointEnd.X - PointStart.X, 2) + Math.Pow(PointEnd.Y - PointStart.Y, 2));
+        return result;
+    }
+}
---
+++ tests/lesson1/Lesson1LineLengthTests/LineFeat/LineTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using Lesson1LineLength.LineFeat;
+
+namespace Lesson1LineLengthTests.LineFeat;
+
+public class LineTests
+{
+    [Fact]
+    public void TestData()
+    {
+        var point1 = new Point(2, 2);
+        var point2 = new Point(3, 3);
+
+        var line = Line.Create(new Point(1, 1), new Point(5, 5));
+        Assert.Equal(new Point(1, 1), line.PointStart);
+        Assert.Equal(new Point(5, 5), line.PointEnd);
+
+        line.PointStart = point1;
+        line.PointEnd = point2;
+        
+        Assert.Equal(new Point(2, 2), line.PointStart);
+        Assert.Equal(new Point(3, 3), line.PointEnd);
+    }
+
+    [Fact]
+    public void TestFormula()
+    {
+        var point1 = new Point(2, 2);
+        var point2 = new Point(3, 3);
+        var line = Line.Create(point1, point2);
+        var lengthOfLine = Math.Sqrt(Math.Pow(point2.X - point1.X, 2) + Math.Pow(point2.Y - point1.Y, 2));
+
+        var actual = line.Length();
+
+        Assert.Equal(lengthOfLine, actual);
+    }
+}
