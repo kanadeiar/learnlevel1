63adf5bd72f304797d37ef4849cdae6a9a9fdb7e
kanadeiar <kanadeiar@gmail.com>
2024-01-27 20:39:18 +0300

    Приложение обмена реализовано

--- src/lesson1/Lesson1Swap/Program.cs
+++ src/lesson1/Lesson1Swap/Program.cs
@@ -1,8 +1,16 @@
 ﻿using Kanadeiar.Common;
+using Lesson1Swap.SwapFunc;
 
 ConsoleHelper.PrintHeader("Задача № 2", "Задача № 2. Написать программу обмена значениями двух переменных.");
 
+var one = ConsoleHelper.ReadNumberFromConsole<int>("Введите первое число");
+var two = ConsoleHelper.ReadNumberFromConsole<int>("Введите второе число");
 
+var swapper = Swapper.Create(one, two);
 
+swapper.Swap();
+Console.WriteLine($"Результат простого обмена: первое = {swapper.First}, второе = {swapper.Second}");
+swapper.AdvancedSwap();
+Console.WriteLine($"Результат дополнительного обмена: первое = {swapper.First}, второе = {swapper.Second}");
 
 ConsoleHelper.PrintFooter();
