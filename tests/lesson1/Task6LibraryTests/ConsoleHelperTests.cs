a199e55a1c1f3eea3c7c2ef72accbc9400e12a22
kanadeiar <kanadeiar@gmail.com>
2024-01-28 13:13:38 +0300

    Завершение выполнения задачи № 6

---
+++ tests/lesson1/Task6LibraryTests/ConsoleHelperTests.cs
@@ -0,0 +1,24 @@
+﻿using Kanadeiar.Common;
+
+namespace Task6LibraryTests;
+
+public class ConsoleHelperTests
+{
+    [Fact]
+    public void TestPrintHeader()
+    {
+        using var writer = new StringWriter();
+        var old = Console.Out;
+        Console.SetOut(writer);
+
+        ConsoleHelper.PrintHeader("TestTitle", "TestText");
+
+        var text = writer.ToString();
+
+        Assert.Equal("TestTitle", Console.Title);
+        Assert.Contains("TestText", text);
+
+        Console.SetOut(old);
+    }
+
+}
