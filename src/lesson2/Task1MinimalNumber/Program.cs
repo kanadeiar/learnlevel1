cb3f130b7662f5086f4ac8f3009b017a121ce417
kanadeiar <kanadeiar@gmail.com>
2024-01-28 13:42:02 +0300

    Начало выполнения задачи 1

---
+++ src/lesson2/Task1MinimalNumber/Program.cs
@@ -0,0 +1,5 @@
+﻿ConsoleHelper.PrintHeader("Задача № 1", "Задача № 1. Написать метод, возвращающий минимальное из трёх чисел.");
+
+
+
+ConsoleHelper.PrintFooter();
---
+++ tests/lesson2/Task1MinimalNumberTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Task1MinimalNumberTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
