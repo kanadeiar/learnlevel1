41c15372b3abd9e6c54244f88b47942ba7128741
kanadeiar <kanadeiar@gmail.com>
2024-02-02 08:41:38 +0300

    Ввод чисел работает

---
+++ src/lesson2/Task3CyclicNumbersSum/CyclicAdderFunc/Adder.cs
@@ -0,0 +1,44 @@
+﻿namespace Task3CyclicNumbersSum.CyclicAdderFunc;
+
+public class Adder : ICommonAdder
+{
+    public IEnumerable<int> Numbers
+    {
+        get
+        {
+            yield return 1;
+            yield return 100;
+        }
+    }
+
+    private Adder()
+    {
+        
+    }
+
+    public static ICommonAdder CreateInstance()
+    {
+        return new Adder();
+    }
+
+    public bool AddNumber(int number)
+    {
+        return true;
+    }
+}
+
+public interface ICommonAdder : IDataAdder, IAddingAdder
+{
+
+}
+
+public interface IDataAdder
+{
+    public IEnumerable<int> Numbers { get; }
+}
+
+public interface IAddingAdder
+{
+    public bool AddNumber(int number);
+}
+
---
+++ tests/lesson2/Task3CyclicNumbersSumTests/CyclicAdderFunc/AdderTests.cs
@@ -0,0 +1,19 @@
+﻿using Task3CyclicNumbersSum.CyclicAdderFunc;
+
+namespace Task3CyclicNumbersSumTests.CyclicAdderFunc;
+
+public class AdderTests
+{
+    [Fact]
+    public void AddNumber()
+    {
+        IAddingAdder adder = Adder.CreateInstance();
+
+        adder.AddNumber(1);
+        _ = adder.AddNumber(100);
+
+        var numbers = (adder as IDataAdder).Numbers.ToArray();
+        Assert.Equal(1, numbers[0]);
+        Assert.Equal(100, numbers[1]);
+    }
+}
