bf3c9eaf9ac3d4d60edc033839cdba2a36f8e8a0
kanadeiar <kanadeiar@gmail.com>
2024-02-11 22:32:53 +0300

    Задача 2 завершена

---
+++ src/lesson3/Task2Numbers/NumbersFunc/ICommonAdder.cs
@@ -0,0 +1,21 @@
+﻿namespace Task2Numbers.NumbersFunc;
+
+public interface ICommonAdder : IValuesAdder, IAddingAdder, ICalculatingAdder
+{
+
+}
+
+public interface IValuesAdder
+{
+    public IEnumerable<int> Numbers { get; }
+}
+
+public interface IAddingAdder
+{
+    public bool AddNumber(int number);
+}
+
+public interface ICalculatingAdder
+{
+    public int SumPlusOddNumbers();
+}
--- tests/lesson2/Task3CyclicNumbersSumTests/GlobalUsings.cs
+++ tests/lesson2/Task3CyclicNumbersSumTests/GlobalUsings.cs
@@ -1,1 +1,3 @@
+global using FluentAssertions;
+global using Task3CyclicNumbersSum.CyclicAdderFunc;
 global using Xunit;
