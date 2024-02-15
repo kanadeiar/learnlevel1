452e75609b6735a3ccb555b52b120d1aa9e2f083
kanadeiar <kanadeiar@gmail.com>
2024-02-15 23:58:53 +0300

    Начало задачи № 1

---
+++ src/lesson4/Task1CountPairsInArray/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task1CountPairsInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
