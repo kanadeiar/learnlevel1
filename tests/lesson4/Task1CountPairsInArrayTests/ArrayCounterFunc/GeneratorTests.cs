b5341918c6c615e556bac3db6b58454642135df7
kanadeiar <kanadeiar@gmail.com>
2024-02-17 09:53:29 +0300

    Задача 1 выполнена

---
+++ tests/lesson4/Task1CountPairsInArrayTests/ArrayCounterFunc/GeneratorTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Task1CountPairsInArrayTests.ArrayCounterFunc;
+
+public class GeneratorTests
+{
+    [Fact]
+    public void TestGenerate()
+    {
+        var generator = new Generator();
+
+        var actuals = generator.Generate(20);
+
+        actuals.Should().HaveCount(20);
+        foreach (var each in actuals)
+        {
+            each.Should().BeInRange(-10_000, 10_000);
+        }
+    }
+}
