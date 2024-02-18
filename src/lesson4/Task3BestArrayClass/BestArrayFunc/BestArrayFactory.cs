ea41d27837053f8227d65141e6a51f59ea9a7103
kanadeiar <kanadeiar@gmail.com>
2024-02-18 17:35:46 +0300

    Задание 3 выполнено

--- src/lesson4/Task3BestArrayClass/BestArrayFunc/BestArrayFactory.cs
+++ src/lesson4/Task3BestArrayClass/BestArrayFunc/BestArrayFactory.cs
@@ -17,18 +17,25 @@ public class BestArrayFactory()
     public BestArray Create(string fileName, IFile? adapter = null)
     {
         var file = adapter ?? new FileAdapter();
-        if (file.Exists(fileName))
+        var isFileExists = file.Exists(fileName);
+        if (isFileExists)
         {
-            string[] strings = file.ReadAllLines(fileName);
-            var array = new int[strings.Length];
-            for (var i = 0; i < strings.Length; i++)
-            {
-                array[i] = int.Parse(strings[i]);
-            }
+            var array = readArrayFromFile(fileName, file);
             return new BestArray(array);
         }
-        else
-            throw new FileLoadException("Файл отсутствует");
+        throw new FileLoadException("Файл отсутствует");
+    }
+
+    private static int[] readArrayFromFile(string fileName, IFile file)
+    {
+        string[] strings = file.ReadAllLines(fileName);
+        var array = new int[strings.Length];
+        for (var i = 0; i < strings.Length; i++)
+        {
+            array[i] = int.Parse(strings[i]);
+        }
+
+        return array;
     }
 
     public BestArray Create(int start, int step, int size)
