16c22590ae536662f646a1505a41c5b5f47a2bb0
kanadeiar <kanadeiar@gmail.com>
2024-02-25 22:36:14 +0300

    Основа приложения готова

---
+++ src/lesson5/Task2StaticClassMessage/MessageFunc/Message.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Task2StaticClassMessage.MessageFunc;
+
+public static class Message
+{
+    public static IEnumerable<string> GetWordsWhenMinLength(string message, int minLength)
+    {
+        var words = message.Split(' ');
+        var filterWords = words.Where(w => w.Length <= minLength).Distinct();
+        return filterWords;
+    }
+
+    public static string MessageWithRemovedWords(string message, char lastLetter)
+    {
+        var wordsRegex = new Regex($@"\w+{lastLetter}\b");
+        var withoutWords = wordsRegex.Replace(message, string.Empty);
+        var spaceRegex = new Regex("[ ]{2,}");
+        var result = spaceRegex.Replace(withoutWords, " ").Trim();
+        return result;
+    }
+
+    public static string LongestWord(string message)
+    {
+        var words = message.Split(' ');
+        Array.Sort(words, (s, s1) => s.Length - s1.Length);
+        return words.Last();
+    }
+
+    public static string LongWordsMessage(string message)
+    {
+        var max = LongestWord(message).Length;
+        var regex = new Regex(@"\w{" + max + @"}\b");
+        var result = new StringBuilder();
+        var collection = regex.Matches(message);
+        foreach (var elem in collection)
+        {
+            result.Append($"{elem} ");
+        }
+
+        return result.ToString().Trim();
+    }
+
+    public static IDictionary<string, int> FrequencyAnalysisDictionary(string message)
+    {
+        var result = new Dictionary<string, int>();
+        var words = message.Split(' ');
+        foreach (var elem in words)
+        {
+            if (result.TryAdd(elem, 1) == false)
+            {
+                result[elem]++;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ tests/lesson5/Task2StaticClassMessageTests/MessageFunc/MessageTests.cs
@@ -0,0 +1,54 @@
+﻿namespace Task2StaticClassMessageTests.MessageFunc;
+
+public class MessageTests
+{
+    [Theory, MemberData(nameof(MessageSource))]
+    public void TestGetWordsWhenMinLength(string message)
+    {
+        var actual = Message.GetWordsWhenMinLength(message, 6).ToArray();
+
+        actual.Should().HaveCount(6);
+        actual.Should().Contain(["Is", "this", "one", "good", "var"]);
+    }
+
+    [Theory, MemberData(nameof(MessageSource))]
+    public void TestMessageWithRemovedWords(string message)
+    {
+        var actual = Message.MessageWithRemovedWords(message, 's');
+
+        actual.Should().Be("one good good message processing insumation message good var");
+    }
+
+    [Theory, MemberData(nameof(MessageSource))]
+    public void TestLongestWord(string message)
+    {
+        var actual = Message.LongestWord(message);
+
+        actual.Should().Be("insumation");
+    }
+
+    [Theory, MemberData(nameof(MessageSource))]
+    public void TestLongWordsMessage(string message)
+    {
+        var actual = Message.LongWordsMessage(message);
+
+        actual.Should().Be("processing insumation");
+    }
+
+    [Theory, MemberData(nameof(MessageSource))]
+    public void TestFrequencyAnalysisDictionary(string message)
+    {
+        var expected = new[] { ("Is", 1), ("message", 2), ("good", 3) }
+            .Select(x => new KeyValuePair<string, int>(x.Item1, x.Item2));
+
+        var actual = Message.FrequencyAnalysisDictionary(message);
+
+        actual.Should().HaveCount(9);
+        actual.Should().Contain(expected);
+    }
+
+    public static IEnumerable<object[]> MessageSource()
+    {
+        yield return new object[] { "Is this one good good message processing lorems insumation lorems message good var" };
+    }
+}
