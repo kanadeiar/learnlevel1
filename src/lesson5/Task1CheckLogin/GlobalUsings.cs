e61221e287a5ba507146e8d97e2973f94f78ab0b
kanadeiar <kanadeiar@gmail.com>
2024-02-25 17:34:08 +0300

    Завершение 1 задания

--- src/lesson5/Task1CheckLogin/GlobalUsings.cs
+++ src/lesson5/Task1CheckLogin/GlobalUsings.cs
@@ -1,4 +1,6 @@
 // Global using directives
 
 global using System.Text.RegularExpressions;
+global using Kanadeiar.Common.Console;
+global using Task1CheckLogin.CheckLoginFunc;
 global using Task1CheckLogin.CheckLoginFunc.CheckBase;
--- src/lesson5/Task1CheckLogin/Program.cs
+++ src/lesson5/Task1CheckLogin/Program.cs
@@ -1,6 +1,4 @@
-﻿using Kanadeiar.Common.Console;
-
-namespace Task1CheckLogin;
+﻿namespace Task1CheckLogin;
 
 internal class Program
 {
@@ -8,7 +6,17 @@ internal class Program
     {
         ConsoleHelper.PrintHeader("Задача № 1", "Задача № 1. Проверка логина.");
 
+        var login = ConsoleHelper.ReadLineFromConsole("Введите логин для проверки");
+        
+        Console.WriteLine("Простая проверка");
+        var simple = Checker.Create(CheckCode.Simple);
+        var simpleIsCorrect = simple.Check(login);
+        Console.WriteLine(simpleIsCorrect ? "Введенный логин корректный" : "Введнный логин неправильный");
 
+        Console.WriteLine("Проверка с регулярным выражением");
+        var regex = Checker.Create(CheckCode.WithRegex);
+        var regexIsCorrect = regex.Check(login);
+        Console.WriteLine(regexIsCorrect ? "Введенный логин корректный" : "Введнный логин неправильный");
 
         ConsoleHelper.PrintFooter();
     }
