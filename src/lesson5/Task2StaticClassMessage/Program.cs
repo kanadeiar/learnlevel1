ac8d6a533bf498592efbff9f505bec5c436a830e
kanadeiar <kanadeiar@gmail.com>
2024-02-25 17:45:39 +0300

    Начало задачи 2

---
+++ src/lesson5/Task2StaticClassMessage/Program.cs
@@ -0,0 +1,15 @@
+﻿using Kanadeiar.Common.Console;
+
+namespace Task2StaticClassMessage;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        ConsoleHelper.PrintHeader("Задача № 2", "Задача № 2. Статический класс сообщений.");
+
+
+
+        ConsoleHelper.PrintFooter();
+    }
+}
---
+++ tests/lesson5/Task2StaticClassMessageTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Task2StaticClassMessageTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
