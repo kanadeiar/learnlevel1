5c858fda633ee09ba4fd6e940899a745d6542424
kanadeiar <kanadeiar@gmail.com>
2024-02-26 20:47:24 +0300

    Основа приложения готова

---
+++ src/lesson5/Task3StringComparison/ComparisonFunc/Comparison.cs
@@ -0,0 +1,11 @@
+﻿namespace Task3StringComparison.ComparisonFunc;
+
+public class Comparison
+{
+    public static bool ShuffleComparison(string first, string second)
+    {
+        var orderedFirst = string.Join("", first.OrderBy(c => c).ToArray());
+        var orderedSecond = string.Join("", second.OrderBy(c => c).ToArray());
+        return orderedFirst.Equals(orderedSecond);
+    }
+}
---
+++ tests/lesson5/Task3StringComparisonTests/ComparisonFunc/ComparisonTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Task3StringComparisonTests.ComparisonFunc;
+
+public class ComparisonTests
+{
+    [Theory, MemberData(nameof(WordsSource))]
+    public void TestShuffleComparison(string first, string second, bool expected)
+    {
+        var actual = Comparison.ShuffleComparison(first, second);
+
+        actual.Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> WordsSource()
+    {
+        yield return ["badc", "abcd", true];
+        yield return ["в4ыа1фв2аaaa", "аывфвaaaа124", true];
+        yield return ["ytrewq", "qwerty", true];
+        yield return ["badcs", "aabcd", false];
+        yield return ["ba", "ass1123abcd", false];
+    }
+}
