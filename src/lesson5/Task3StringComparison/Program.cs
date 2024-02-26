633e98141d81cb4896d944e59a290abced88abae
kanadeiar <kanadeiar@gmail.com>
2024-02-26 20:12:44 +0300

    Начало задача № 3

---
+++ src/lesson5/Task3StringComparison/Program.cs
@@ -0,0 +1,15 @@
+﻿using Kanadeiar.Common.Console;
+
+namespace Task3StringComparison;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        ConsoleHelper.PrintHeader("Задача № 3", "Задача № 3. Сравнение двух строк с перестановкой.");
+
+
+
+        ConsoleHelper.PrintFooter();
+    }
+}
