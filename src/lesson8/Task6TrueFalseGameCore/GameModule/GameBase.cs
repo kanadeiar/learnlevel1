11b2877f7ce1bda1166297817d0fcc21f964551a
kanadeiar <kanadeiar@gmail.com>
2024-07-12 23:33:03 +0300

    Рефакторинг - методы вынесены в дочерний подкласс

---
+++ src/lesson8/Task6TrueFalseGameCore/GameModule/GameBase.cs
@@ -0,0 +1,103 @@
+﻿namespace Task6TrueFalseGameCore.GameModule;
+
+public class GameBase
+{
+    public const int SIZE_QUESTION = 3;
+
+    private readonly GameManager _manager;
+
+    private GameMode _mode;
+    
+    public string QuestionText
+    {
+        get
+        {
+            var text = _mode switch
+            {
+                GameMode.Welcome => "Добро пожаловать в игру!\nНажмите пожалуйства кнопку для начала игры!",
+                GameMode.Question => _manager.Current.Text,
+                GameMode.Victory => "Поздравления!\nВы выиграли игру!\nНажмите на кнопку для повторной игры.",
+                GameMode.GameOver => $"Вы проиграли игру!\nВы правильно ответили только на {_manager.countTrueAnswers} вопроса из {SIZE_QUESTION}",
+                _ => string.Empty,
+            };
+
+            return text;
+        }
+    }
+
+    public bool EnableStart =>
+        _mode switch
+        {
+            GameMode.Question => false,
+            _ => true,
+        };
+
+    public bool EnableAnswer =>
+        _mode switch
+        {
+            GameMode.Question => true,
+            _ => false,
+        };
+
+    public GameBase(GameManager manager, GameMode mode = GameMode.Welcome)
+    {
+        _manager = manager;
+        _mode = mode;
+    }
+
+    public void Start()
+    {
+        _mode = GameMode.Question;
+        var rnd = new Random();
+        _manager.generedQuestions = new int[SIZE_QUESTION];
+        var randNumbers = Enumerable.Range(0, _manager.allQuestions.Count)
+            .Select(x => x)
+            .ToList();
+        for (var i = 0; i < SIZE_QUESTION; i++)
+        {
+            var r = rnd.Next(0, randNumbers.Count);
+            _manager.generedQuestions[i] = randNumbers[r];
+            randNumbers.RemoveAt(r);
+        }
+
+        _manager.currentQuestion = 0;
+        _manager.countTrueAnswers = 0;
+    }
+
+    public void Yes()
+    {
+        if (_manager.Current.IsTrue)
+        {
+            _manager.countTrueAnswers++;
+        }
+
+        answer();
+    }
+
+    public void No()
+    {
+        if (_manager.Current.IsTrue == false)
+        {
+            _manager.countTrueAnswers++;
+        }
+
+        answer();
+    }
+
+    private void answer()
+    {
+        _manager.currentQuestion++;
+
+        if (_manager.currentQuestion > SIZE_QUESTION - 1)
+        {
+            if (_manager.countTrueAnswers == SIZE_QUESTION)
+            {
+                _mode = GameMode.Victory;
+            }
+            else
+            {
+                _mode = GameMode.GameOver;
+            }
+        }
+    }
+}
--- src/lesson8/Task6TrueFalseGameCore/GameModule/GameManager.cs
+++ src/lesson8/Task6TrueFalseGameCore/GameModule/GameManager.cs
@@ -8,59 +8,34 @@ namespace Task6TrueFalseGameCore.GameModule;
 
 public class GameManager : ModelBase
 {
-    private const int SIZE_QUESTION = 3;
-    
     private readonly string _fileName;
-    
-    private GameMode _mode;
+
+    internal GameBase game;
 
     internal List<Question> allQuestions = new ();
 
     internal int[] generedQuestions = Array.Empty<int>();
 
-    private int _currentQuestion = 0;
+    internal Question Current => allQuestions[generedQuestions[currentQuestion]];
 
-    private int _countTrueAnswers = 0;
+    internal int currentQuestion = 0;
+
+    internal int countTrueAnswers = 0;
     
     internal static IXmlFileSerializer<List<Question>>? serializer;
 
-    public string QuestionText
-    {
-        get
-        {
-            var text = _mode switch
-            {
-                GameMode.Welcome => "Добро пожаловать в игру!\nНажмите пожалуйства кнопку для начала игры!",
-                GameMode.Question => allQuestions[generedQuestions[_currentQuestion]].Text,
-                GameMode.Victory => "Поздравления!\nВы выиграли игру!\nНажмите на кнопку для повторной игры.",
-                GameMode.GameOver => $"Вы проиграли игру!\nВы правильно ответили только на {_countTrueAnswers} вопроса из {SIZE_QUESTION}",
-                _ => string.Empty,
-            };
+    public string QuestionText => game.QuestionText;
 
-            return text;
-        }
-    }
+    public bool EnableStart => game.EnableStart;
 
-    public bool EnableStart =>
-        _mode switch
-        {
-            GameMode.Question => false,
-            _ => true,
-        };
-
-    public bool EnableAnswer =>
-        _mode switch
-        {
-            GameMode.Question => true,
-            _ => false,
-        };
+    public bool EnableAnswer => game.EnableAnswer;
 
     public GameManager(string fileName)
     {
         _fileName = fileName;
         allQuestions = Load();
 
-        _mode = GameMode.Welcome;
+        game = new GameBase(this);
         NotifyObservers();
     }
 
@@ -79,61 +54,19 @@ public class GameManager : ModelBase
 
     public void Start()
     {
-        _mode = GameMode.Question;
-        var rnd = new Random();
-        generedQuestions = new int[SIZE_QUESTION];
-        var randNumbers = Enumerable.Range(0, allQuestions.Count)
-            .Select(x => x)
-            .ToList();
-        for (var i = 0; i < SIZE_QUESTION; i++)
-        {
-            var r = rnd.Next(0, randNumbers.Count);
-            generedQuestions[i] = randNumbers[r];
-            randNumbers.RemoveAt(r);
-        }
-
-        _currentQuestion = 0;
-        _countTrueAnswers = 0;
-
+        game.Start();
         NotifyObservers();
     }
 
     public void Yes()
     {
-        if (allQuestions[generedQuestions[_currentQuestion]].IsTrue)
-        {
-            _countTrueAnswers++;
-        }
-
-        answer();
+        game.Yes();
         NotifyObservers();
     }
 
     public void No()
     {
-        if (allQuestions[generedQuestions[_currentQuestion]].IsTrue == false)
-        {
-            _countTrueAnswers++;
-        }
-
-        answer();
+        game.No();
         NotifyObservers();
-    }
-
-    private void answer()
-    {
-        _currentQuestion++;
-
-        if (_currentQuestion > SIZE_QUESTION - 1)
-        {
-            if (_countTrueAnswers == SIZE_QUESTION)
-            {
-                _mode = GameMode.Victory;
-            }
-            else
-            {
-                _mode = GameMode.GameOver;
-            }
-        }
     }
 }
